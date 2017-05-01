da0758efa105e695bbc97f3468b3c5b117e92ba8
ovo4096 <ovo4096@gmail.com>
2017-05-01 14:18:34 +0800

    Move decodeLocation to utils class

--- src/utils.js
+++ src/utils.js
@@ -2,9 +2,44 @@ const http = require('http')
 const cheerio = require('cheerio')
 const url = require('url')
 const Song = require('./song')
-const decodeLocation = require('./decode-location')
 
 module.exports = class {
+  /**
+   * The decoding method is implemented by https://github.com/Flowerowl/xiami/blob/master/xiami.php
+   */
+  static decodeLocation (location) {
+    let loc2 = parseInt(location.substr(0, 1))
+    let loc3 = location.substr(1)
+    let loc4 = Math.floor(loc3.length / loc2)
+    let loc5 = loc3.length % loc2
+    let loc6 = []
+    let loc7 = 0
+    let loc8 = ''
+    let loc9 = ''
+    let loc10 = ''
+    while (loc7 < loc5) {
+      loc6[loc7] = loc3.substr((loc4 + 1) * loc7, loc4 + 1)
+      loc7++
+    }
+    loc7 = loc5
+    while (loc7 < loc2) {
+      loc6[loc7] = loc3.substr(loc4 * (loc7 - loc5) + (loc4 + 1) * loc5, loc4)
+      loc7++
+    }
+    loc7 = 0
+    while (loc7 < loc6[0].length) {
+      loc10 = 0
+      while (loc10 < loc6.length) {
+        loc8 += loc6[loc10][loc7] !== undefined ? loc6[loc10][loc7] : ''
+        loc10++
+      }
+      loc7++
+    }
+
+    loc9 = decodeURIComponent(loc8).replace(/\^/g, '0')
+    return loc9
+  }
+
   static getSongInfo (id) {
     return new Promise((resolve, reject) => {
       http.get(`http://www.xiami.com/song/${id}`, (res) => {
@@ -70,7 +105,7 @@ module.exports = class {
             const parsedData = JSON.parse(rawData)
             let audioURL = null
             if (parsedData.status === 1 && parsedData.location !== '') {
-              audioURL = url.parse(decodeLocation(parsedData.location))
+              audioURL = url.parse(this.decodeLocation(parsedData.location))
             }
             resolve(audioURL)
           } catch (e) {
--- test/index.js
+++ test/index.js
@@ -2,6 +2,12 @@ const xiami = require('../src')
 
 xiami.utils.getSong('xLJhMEa6116').then((song) => {
   console.log(`${song.name} (id: ${song.id})`)
-}).catch((err) => {
-  console.log(err)
+}).catch((e) => {
+  console.log(e)
+})
+
+xiami.utils.getSongInfo('xLJhMEa6116').then((info) => {
+  console.log(info)
+}).catch((e) => {
+  console.log(e)
 })
